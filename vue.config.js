d27f0865acfb0e125629de4c0834fa2b39b3fb9c
RichardBarreda <barredarichardmarck@gmail.com>
2024-06-22 13:47:43 +0800

    changes in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-// vue.config.js file to be placed in the root of your repository
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-portfolio/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/my-portfolio/" : "/",
+})
