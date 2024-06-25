bb3482cf40f48e0499f48fe011a16803d508ef04
RichardBarreda <barredarichardmarck@gmail.com>
2024-06-26 00:03:52 +0800

    edit content of work history

--- src/components/WorkHistory.vue
+++ src/components/WorkHistory.vue
@@ -3,17 +3,61 @@
       <Work :workid="workId1" :company="workCompany1" :date="workDate1" :website="workWeb1" :address="workAddress1" :position="workPosition1" :gallery="workGallery1">
 
         <template v-slot:bullet>
+          <p class="work-subtitle">IT Support</p>
           <ul>
-            <li class="bullet-items">Tincidunt vitae semper quis lectus nulla at. Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque.</li>
-            <li class="bullet-items">Sagittis purus sit amet volutpat consequat mauris nunc. Eleifend mi in nulla posuere sollicitudin aliquam.</li>
-            <li class="bullet-items">Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. </li>
-            <li class="bullet-items">Varius vel pharetra vel turpis nunc eget. Eu augue ut lectus arcu bibendum at varius vel pharetra.</li>
-            <li class="bullet-items">Mus mauris vitae ultricies leo integer malesuada nunc. Lorem ipsum dolor sit amet consectetur adipiscing elit duis.</li>
-            <li class="bullet-items">Orci dapibus ultrices in iaculis nunc sed. Consequat interdum varius sit amet mattis vulputate enim.</li>
-            <li class="bullet-items">Sit amet risus nullam eget. Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Enim facilisis gravida neque convallis a cras semper.</li>
-            <li class="bullet-items">Volutpat sed cras ornare arcu. Arcu ac tortor dignissim convallis aenean. </li>
-            <li class="bullet-items">Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Dui id ornare arcu odio ut sem nulla.</li>
-            <li class="bullet-items">Dapibus ultrices in iaculis nunc sed augue. Mi quis hendrerit dolor magna eget est lorem. Ultricies leo integer malesuada nunc vel risus commodo.</li>
+            <li class="bullet-items">
+              Provide technical support and troubleshooting assistance to employees via 
+              ticketing system, email, chat, remote application or in person.
+            </li>
+            <li class="bullet-items">
+              Diagnose and resolve hardware and software problems on time, such as 
+              hardware replacement, software error, and connectivity issues. Escalate 
+              complex problems to IT Manager when necessary to avoid downtime on the 
+              end-user.
+            </li>
+            <li class="bullet-items">
+              Maintain accurate records of support request with detailed solution provided 
+              that can serve as a guideline for the IT Team.
+            </li>
+            <li class="bullet-items">
+              Manage user accounts through Microsoft Active Directory 2008. This includes 
+              Account Creation, Reset Account Password, and User Group Membership.
+            </li>
+            <li class="bullet-items">
+              Setup company laptop for newly hired employees and conduct IT related 
+              training including the importance of Information Security in the workplace.
+            </li>
+            <li class="bullet-items">
+              Promote cyber security in the workplace through email campaign and provide 
+              training to required employees as needed.
+            </li>
+            <li class="bullet-items">
+              Conduct annual ISMS Internal audits to the departments of the company and 
+              ensuring that they comply to the company’s Information Security rules.
+            </li>
+            <li class="bullet-items">
+              Mentor and train other Junior IT members. Ensures that the company 
+              standard practices and policies are adhered to.
+            </li>
+          </ul>
+          <p class="work-subtitle">Web / System Developer</p>
+          <ul>
+            <li class="bullet-items">
+              Develop a Company Website for Nanox Philippines Inc. 
+              https://www.nanoxphilippines.com
+            </li>
+            <li class="bullet-items">
+              Maintain the existing company system such as Online Leave Application, 
+              Document Management System, and Employee Database System.
+            </li>
+            <li class="bullet-items">
+              Develop an internal system called “Cashless payment System” that allows the 
+              employees to pay their bills on the canteen with the use of RFID Card.
+            </li>
+            <li class="bullet-items">
+              Develop and implemented an internal IT Ticketing system that the company
+              employees use to submit support request to the IT Department.
+            </li>
           </ul>
           
         </template>
@@ -24,16 +68,33 @@
 
         <template v-slot:bullet>
           <ul>
-            <li class="bullet-items">Tincidunt vitae semper quis lectus nulla at. Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque.</li>
-            <li class="bullet-items">Sagittis purus sit amet volutpat consequat mauris nunc. Eleifend mi in nulla posuere sollicitudin aliquam.</li>
-            <li class="bullet-items">Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. </li>
-            <li class="bullet-items">Varius vel pharetra vel turpis nunc eget. Eu augue ut lectus arcu bibendum at varius vel pharetra.</li>
-            <li class="bullet-items">Mus mauris vitae ultricies leo integer malesuada nunc. Lorem ipsum dolor sit amet consectetur adipiscing elit duis.</li>
-            <li class="bullet-items">Orci dapibus ultrices in iaculis nunc sed. Consequat interdum varius sit amet mattis vulputate enim.</li>
-            <li class="bullet-items">Sit amet risus nullam eget. Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Enim facilisis gravida neque convallis a cras semper.</li>
-            <li class="bullet-items">Volutpat sed cras ornare arcu. Arcu ac tortor dignissim convallis aenean. </li>
-            <li class="bullet-items">Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Dui id ornare arcu odio ut sem nulla.</li>
-            <li class="bullet-items">Dapibus ultrices in iaculis nunc sed augue. Mi quis hendrerit dolor magna eget est lorem. Ultricies leo integer malesuada nunc vel risus commodo.</li>
+            <li class="bullet-items">
+              Implemented websites and applications from concept through deployment.
+            </li>
+            <li class="bullet-items">
+              Convert mock-up designs into responsive and reusable front-end code then 
+              integrate it into WordPress template.
+            </li>
+            <li class="bullet-items">
+              Tested websites and performed troubleshooting prior to deployment to 
+              ensure that the website meets the client’s expectations.
+            </li>
+            <li class="bullet-items">
+              Designed websites to be compatible with top browsers including Google 
+              Chrome, Firefox, Microsoft Edge, and Safari.
+            </li>
+            <li class="bullet-items">
+              Building reusable components and optimized them for maximum 
+              performance across a vast array of web-capable devices and browsers.
+            </li>
+            <li class="bullet-items">
+              Uses GitHub to implement version controls with the websites and 
+              collaborates with different developers from the team.
+            </li>
+            <li class="bullet-items">
+              Coordinate with QA team and ensure that any website issues or bugs that 
+              arise will be fixed immediately.
+            </li>
           </ul>
           
         </template>
@@ -79,7 +140,7 @@
           workDate1: "October 2020 - Present",
           workWeb1: "https://nanoxphilippines.com/",
           workAddress1: "Clark Freeport, Mabalacat, Pampanga",
-          workPosition1: "IT Support / Web Developer",
+          workPosition1: "IT Support / System Developer",
           workGallery1: [
             { key:"img1", location: require('../assets/nanox-img-1.jpg'), date: "October 16, 2022", caption: "Current IT Staff with OJT on a very ordinary day!"},
             { key:"img2", location: require('../assets/nanox-img-2.jpg'), date: "June 12, 2022", caption: "In vulputate ex et faucibus elementum. Nunc nisi libero, accumsan!"},
@@ -129,9 +190,18 @@
   list-style-type:square;
 }
 
+.work-subtitle {
+  font-size: 18px;
+  font-weight: 500;
+}
+
 @media (min-width: 576px) and (max-width: 767.98px) {
   .bullet-items {
     font-size: 14px;
+  }
+
+  .work-subtitle {
+    font-size: 15px;
   }
 }
 
@@ -139,6 +209,10 @@
   .bullet-items {
     font-size: 14px;
   } 
+
+  .work-subtitle {
+    font-size: 15px;
+  }
 }
 </style>
   
